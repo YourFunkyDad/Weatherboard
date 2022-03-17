c2e42edeaf9f7fa67f364a2ac1529e569a245f2b
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-03-17 13:24:49 -0400

    Adding API Key

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const apiKey = "f8bdee5d7df0d8080f755ec842a46146";
+var userFormEl = $("#citySearch");
+
+// API Call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f8bdee5d7df0d8080f755ec842a46146
+
